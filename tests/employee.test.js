552be8d9cc0e4fab9563f48e8809a6fa2394004d
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-05-07 19:07:49 -0700

    did not realize that my first attempts att making my tests was this bad i did not have a single actual test written out

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,28 +1,34 @@
-const { default: expect } = require("expect");
-const { describe } = require("yargs");
+// const { default: expect } = require("expect");
+// const { describe } = require("yargs");
+const { expect } = require('@jest/globals');
 const Employee = require("../lib/Employee");
 
 
-describe('Employee', ()=>{
-    const name = 'Justin';
-    const id = 345;
-    const email = 'puppyface1029w@yahoo.com';
+describe('Employee', () => {
 
-    const employee = new Employee(name, id, email); 
-    
-    it('it should return the name of our employee', () =>{
-        expect(employee.getName().toBe('Justin'));
-    });
-    it('it should return the role of our employee', () =>{
-        expect(employee.getRole().toBe('Manager'));
-    });
-    it('it should return the id of our employee', () =>{
-        expect(employee.getId().toBe('345'));
-    });
-    it('it should return the role of our employee', () =>{
-        expect(employee.getEmail().toBe('puppyface1029w@yahoo.com'));
+    test('create an instance of an employee object and see if it is indeed an object', () => {
+        const employee = new Employee();
+        expect(typeof (employee)).toBe("object");
     });
 
+    test("we need to make sure that the employees name is indeed being retrieved", () => {
+        const employee = new Employee('Justin');
+        expect(employee.getName()).toBe('Justin');
+    });
 
+    test("we need to make sure that the employees id is indeed being retrieved", () => {
+        const employee = new Employee('Justin', '1234');
+        expect(employee.getId()).toBe('1234');
+    });
 
-})
+    test("we need to make sure that the employees email is indeed being retrieved", () => {
+        const employee = new Employee('Justin', '1234', 'jengutierrez.1210@gmail.com');
+        expect(employee.getEmail()).toBe('jengutierrez.1210@gmail.com');
+    });
+
+    test("we need to make sure that the employees role is indeed being retrieved", () => {
+        const employee = new Employee('Employee');
+        expect(employee.getRole()).toBe('Employee');
+    });
+
+});
