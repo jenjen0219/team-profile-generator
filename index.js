d0e5af1cf3cd02cc982c563414fd553443c69a3e
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-05-07 18:04:36 -0700

    had some help from a friend where they made me realize i had created my generated html incorrectly so the index html i was working on was tweaked and reworked within the generate html js in src

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import inquirer 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHTML = require('./src/main.html');
+const generateHTML = require('./src/generateHTML');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
